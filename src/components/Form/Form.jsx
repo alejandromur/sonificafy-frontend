8eec7b5803255e46503bc9895f6ccdbaf1b9d59c
Alejandro Mur <alejandro@mamutlove.com>
2025-03-29 17:14:53 +0100

    feat: render server response

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,20 +1,30 @@
+import { useState } from "react";
+
 export default function Form() {
+  const [audioData, setAudioData] = useState({});
+
   const handleSubmit = async (event) => {
-    console.log(event.target[0].value, new FormData());
     event.preventDefault();
-    const rawResponse = await fetch("http://localhost:3000/api/sonification", {
-      method: "POST",
-      mode: "cors",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      // body: new FormData(),
-      body: JSON.stringify({
-        url: event.target[0].value,
-      }),
-    });
-    console.log(rawResponse);
+    try {
+      const rawResponse = await fetch(
+        `${import.meta.env.VITE_API_BASE_URL}/sonification`,
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            url: event.target[0].value,
+          }),
+        }
+      );
+      const data = await rawResponse.json();
+      setAudioData(data.processingInfo);
+    } catch (error) {
+      console.error("Error al procesar la solicitud:", error);
+    }
   };
 
   return (
@@ -26,9 +36,25 @@ export default function Form() {
           type="text"
           inputMode="url"
           placeholder="https://example.com"
-          value="https://example.com"
         />
       </form>
+      {audioData ? (
+        <ul>
+          {audioData.fileName ? <li>{audioData.fileName}</li> : null}
+          {audioData.originalUrl ? (
+            <li>
+              <a
+                href={audioData.originalUrl}
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                {audioData.originalUrl}
+              </a>
+            </li>
+          ) : null}
+          {audioData.timestamp ? <li>{audioData.timestamp}</li> : null}
+        </ul>
+      ) : null}
     </>
   );
 }
