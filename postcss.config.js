2e40e522c0e4e9477e6f30a07f9e9c9747125fb9
Alejandro Mur <alejandro@mamutlove.com>
2025-03-31 14:06:50 +0200

    build: update Vite and PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
+import postcssNested from "postcss-nested";
+import postcssPresetEnv from "postcss-preset-env";
+import autoprefixer from "autoprefixer";
+
 export default {
-  plugins: {
-    "postcss-preset-env": {
-      stage: 1,
-    },
-    autoprefixer: {},
-  },
+  plugins: [postcssNested, postcssPresetEnv({ stage: 1 }), autoprefixer],
 };
